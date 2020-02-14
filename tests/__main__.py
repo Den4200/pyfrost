957044096a4891a04da34933ce1b9e5f0c83d56f
Den4200 <dpham.42@hotmail.com>
2020-02-14 11:37:19 -0500

    add init_db as an option

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,20 +1,24 @@
 from sys import argv
-from .run_server import run_server
-from .run_client import run_client
-# from frost.server.database import init_db
+
 
 if __name__ == "__main__":
     # init_db()
     if len(argv) == 2:
 
         if argv[1] == 'server':
+            from .run_server import run_server
             run_server()
 
         elif argv[1] == 'client':
+            from .run_client import run_client
             run_client()
 
+        elif argv[1] == 'init_db':
+            from frost.server.database import init_db
+            init_db()
+
         else:
-            print('Usage: python -m tests <server or client>')
+            print('Usage: python -m tests <server / client / init_db>')
 
     else:    
-        print('Usage: python -m tests <server or client>')
+        print('Usage: python -m tests <server / client / init_db>')
