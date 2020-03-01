c9b0099456cf869a0b6870044c2f3bd16299ddf2
Den4200 <dpham.42@hotmail.com>
2020-03-01 12:18:13 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name="pyfrost",
+    version="0.0.1",
+    author="Dennis Pham",
+    author_email="dpham.42@hotmail.com",
+    description="An online chat library for creating the client and server.",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Den4200/pyfrost",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+
+        "Topic :: Communications ",
+        "Topic :: Communications :: Chat"
+    ],
+    python_requires='>=3.6',
+)
