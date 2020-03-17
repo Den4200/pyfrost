b76988b10ee8ee394671fbc36542c735654dc29c
Den4200 <dpham.42@hotmail.com>
2020-03-17 16:35:03 -0400

    v0.1.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2020 Dennis Pham'
 author = 'Dennis Pham'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
-version = '0.1.3'
+release = '0.1.4'
+version = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
